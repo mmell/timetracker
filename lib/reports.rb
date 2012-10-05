f679123e855d443d2a0936449c6dd7223326d1c8
Michael Mell <mike.mell@nthwave.net>
2012-10-05 10:16:48 -0700

    tweaks

--- lib/reports.rb
+++ lib/reports.rb
@@ -2,11 +2,13 @@ module Reports
   require 'reports/base.rb'
   require 'reports/activity_detail.rb'
   require 'reports/project_summary.rb'
+  require 'reports/project_summary_description.rb'
   require 'reports/daily.rb'
  
   Formats = [
-    ['Activity Detail', 'ActivityDetail'],
     ['Project Summary', 'ProjectSummary'],
+    ['Project Summary & Description', 'ProjectSummaryDescription'],
+    ['Activity Detail', 'ActivityDetail'],
     ['Daily Activity', 'Daily'],
   ]
       
